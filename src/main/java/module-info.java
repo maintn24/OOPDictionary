74b6e94d0e18e5fc80ca8486ae36ae2345aa0406
Mai Nguyễn <maintn124@gmail.com>
2023-10-12 10:14:21 +0700

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module oop.oopdictionary {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens oop.oopdictionary to javafx.fxml;
+    exports oop.oopdictionary;
+}
